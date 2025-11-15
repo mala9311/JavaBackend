b591dfbcbe2240498dcbeb8548ed135872c9b23a
mala9311 <malakumari9311@gmail.com>
2025-11-15 12:36:45 +0530

    backend03

---
+++ OneDrive/Documents/NetBeansProjects/javaBackend03/src/main/java/com/mycompany/javabackend03/JavaBackend03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabackend03;
+
+/**
+ *
+ * @author HP
+ */
+public class JavaBackend03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
