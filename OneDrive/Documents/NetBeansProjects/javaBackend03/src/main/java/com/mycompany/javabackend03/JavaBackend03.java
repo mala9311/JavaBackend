b4212d84f8895c54ccc0158aa0260ed0f00c9020
mala9311 <malakumari9311@gmail.com>
2025-11-20 16:12:38 +0530

    Reversing the array

--- OneDrive/Documents/NetBeansProjects/javaBackend03/src/main/java/com/mycompany/javabackend03/JavaBackend03.java
+++ OneDrive/Documents/NetBeansProjects/javaBackend03/src/main/java/com/mycompany/javabackend03/JavaBackend03.java
@@ -8,9 +8,21 @@ package com.mycompany.javabackend03;
  *
  * @author HP
  */
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class JavaBackend03 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int number[] = {2,4,6,8,9,10};
+        int start = 0;
+        int end = number.length-1;
+        for(int i = number.length -1; i>0; i--){
+            int temp = number[start];
+            number[start] = number[end];
+            number[end] = temp;
+        }
+        System.out.println("Reverse the Arrays:" + Arrays.toString(number));
+            
     }
 }
